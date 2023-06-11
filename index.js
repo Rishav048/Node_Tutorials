e7d0dc69f3dfe461aa16819a50329da2bdb6d58b
Rishav048 <chakrabortyrishav048@gmail.com>
2023-06-11 13:13:33 +0530

    server created

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+// console.log("-->", __filename);
+// console.log("-->", __dirname);
+
+const PORT = 8080;
+
+http
+  .createServer((req, res) => {
+    console.log("Server is runnig at PORT-8080");
+    res.write("<h1> Hi This the Server Testing </h1>");
+    res.end();
+  })
+  .listen(PORT);
