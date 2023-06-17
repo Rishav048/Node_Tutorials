f3437edbb293cde22244602b8ddddca3f5817fc5
Rishav048 <chakrabortyrishav048@gmail.com>
2023-06-17 21:54:57 +0530

    404 Page

--- nefile.js
+++ nefile.js
@@ -4,7 +4,15 @@ const app = express();
 const path = require("path");
 const Public_Path = path.join(__dirname, "Public");
 
-app.use(express.static(Public_Path));
+//app.use(express.static(Public_Path));
+
+app.get("/home", (_, res) => {
+  res.sendFile(`${Public_Path}/home.html`);
+});
+
+app.get("*", (_, res) => {
+  res.sendFile(`${Public_Path}/404.html`);
+});
 
 console.log(Public_Path);
 app.listen(8080);
